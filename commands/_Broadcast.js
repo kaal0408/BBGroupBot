7a27de649fd0906924fc08ab0b13a26bf71c5a03
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2023-10-05 19:03:40 +0530

    Update _Broadcast.js

--- commands/_Broadcast.js
+++ commands/_Broadcast.js
@@ -1,3 +1,16 @@
+/*CMD
+  command: Broadcast
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
 var msg = options.msg
 
 Bot.sendMessage("*📢 Admin Message ~ #msg\n➖➖➖➖➖➖➖➖➖➖➖➖\n\n"+msg+"*")
