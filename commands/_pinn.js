4c083653c0138cfe7b92e536319a595f20541404
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2023-10-05 18:58:43 +0530

    Update _pin.js

--- commands/_pinn.js
+++ commands/_pinn.js
@@ -1,3 +1,17 @@
+/*CMD
+  command: pin
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
+
 var chat_id = options.result.chat.id  
 var message_id= options.result.message_id  
   
